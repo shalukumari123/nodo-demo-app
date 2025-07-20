e7a3896761f11c5f5e2142e355207e3ebae2639f
shalu <shalu.kumari@publicissapient.com>
2025-07-20 21:02:25 +0530

    feat:first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+// Middleware to parse JSON (not required for GET, but good to have)
+app.use(express.json());
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('Hello from Express Demo App!');
+});
+
+// Ping route
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+// New route: Get name info
+app.get('/name', (req, res) => {
+  res.json({
+    firstName: 'Shalu',
+    lastName: 'Kumari',
+    role: 'Backend Developer'
+  });
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
